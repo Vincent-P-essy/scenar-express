7fec1d497a0f23e07c5513c56b144c3b8bc84001
Vincent Plessy <111693059+Vincent-P-essy@users.noreply.github.com>
2026-01-30 13:26:50 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = "scenar-express-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).then((resp) => {
+          // cache au passage (pratique si tu ajoutes des fichiers plus tard)
+          const copy = resp.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+          return resp;
+        }).catch(() => cached)
+      );
+    })
+  );
+});
