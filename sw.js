efb38da94ac1368f88e9e69047d1bddc10abb6e2
Vincent Plessy <111693059+Vincent-P-essy@users.noreply.github.com>
2026-02-06 12:26:57 +0100

    Update service worker cache version to v117

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "scenar-express-v116";
+const CACHE = "scenar-express-v117";
 
 // Mets ici TOUT ce qui doit marcher offline direct
 const PRECACHE = [
@@ -83,6 +83,7 @@ async function cacheFirst(req) {
     return cached || Response.error();
   }
 }
+
 
 
 
